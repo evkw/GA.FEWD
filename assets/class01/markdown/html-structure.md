2354027d27b7a637a1f9f5eb7dab8bbb6124f05f
Evan Wallace <ewallace@berkleyre.com.au>
2018-01-15 09:23:40 +1100

    Adding more content to class 1

---
+++ assets/class01/markdown/html-structure.md
@@ -0,0 +1,14 @@
+### HTML Structure example
+
+```html
+    <!doctype html>
+    <html>
+        <head>
+            <title>HTML Structure Example</title>
+        </head>
+        <body>
+            <h1>This is the visible part of the web page</h1>
+        </body>
+    </html>
+
+```
--- js/main.js
+++ js/main.js
@@ -13,21 +13,9 @@ function initReveal () {
 
 		// Optional reveal.js plugins
 		dependencies: [
-			// { src: '../lib/js/classList.js', condition: function() { return !document.body.classList; } },
-			// { src: '../plugin/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-			// { src: '../plugin/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-			// { src: '../plugin/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-			// override highlight plugin in favor of actual hightlight.js library; needed for reveal-code-focus.js
-			// { src: '../node_modules/highlight.js/lib/highlight.js' },
-			// {
-			// 	src: '../node_modules/reveal-code-focus/reveal-code-focus.js',
-			// 	async: true,
-			// 	callback: function() {
-			// 		RevealCodeFocus();
-			// 	}
-			// },
-			// { src: '../plugin/zoom-js/zoom.js', async: true },
-			// { src: '../plugin/notes/notes.js', async: true }
+			{ src: 'js/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+			{ src: 'js/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+			{ src: 'js/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
 		],
 
 	}
